3d8fcecc9312a987dbb058a5c2d98a40bfb95752
iamnubs <ghuniyu.fattah.rozaq@gmail.com>
2018-11-07 16:08:28 +0700

    Fix about

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -1,19 +1,18 @@
 @extends('masters.nav')
 @section('content')
     <div class="row">
-        <div class="col-12">
-
+        <div class="col-10 offset-1">
             <div class="card text-center">
                 <div class="card-header">About</div>
                 <div class="card-body" style="padding-left: 10%!important;">
-                    <h1 class="card-title">Welcome to {{$about['title']}}</h1>
-                    <h4 class="card-title mt-5">This event will be held from {{$about['start']}}
-                        to {{$about['finish']}}</h4>
+                    <h2 class="card-title">Welcome to {{$about['title']}}</h2>
+                    <h5 class="card-title mt-5">This event will be held from {{$about['start']}}
+                        to {{$about['finish']}}</h5>
                     @if($about['freeze'] > 0)
-                        <h4 class="card-title">and Scoreboard will be Frozen {{$about['freeze']}} Hour(s) before the end of the
-                            Game</h4>
+                        <h5 class="card-title">and Scoreboard will be Frozen {{$about['freeze']}} Hour(s) before the end
+                            of the Game</h5>
                     @endif
-                    <div class="text-left">{!!$about['description'] !!}</div>
+                    <div class="text-left mt-5">{!!$about['description'] !!}</div>
                 </div>
             </div>
 
